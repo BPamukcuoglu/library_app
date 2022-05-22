e7e3365a3e446060ac2c4ae47c5ed89242c3190e
BPamukcuoglu <bedirhanpamukcuoglu@gmail.com>
2022-05-22 19:12:36 +0300

    readme updated & env added

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Awesome Project Build with TypeORM
+# Inventa Analytics Library Application Project Build with TypeORM
+
+
+This project uses mySQL as a relational database. 
+You have to set PORT, USERNAME and PASSWORD in order to run the application.
+Schema and tables will be automatically generated.
 
 Steps to run this project:
 
 1. Run `npm i` command
-2. Setup database settings inside `data-source.ts` file
+2. Set database settings inside `.env` file
 3. Run `npm start` command
--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,14 @@ import { Book } from "./entity/Book"
 import { BookBelonging } from "./entity/BookBelonging"
 import { BookRate } from "./entity/BookRate"
 import { User } from "./entity/User"
-
+require('dotenv').config({ path: '.env' })
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "13241324",
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: "library_app",
     synchronize: true,
     logging: false,
